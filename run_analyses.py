bc66ebf9b8bd1fe1433d90aad7b026f05d2aa30b
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-08-31 12:47:58 +0100

    Update

---
+++ run_analyses.py
@@ -0,0 +1,6 @@
+from src.analysis.decisions import perform_analysis_decisions
+
+perform_analysis_decisions('completions/decisions_desc_gpt-3.5-turbo-0301.csv', 'results/decisions_desc_gpt-3.5-turbo-0301')
+perform_analysis_decisions('completions/decisions_pres_gpt-3.5-turbo-0301.csv', 'results/decisions_pres_gpt-3.5-turbo-0301')
+perform_analysis_decisions('completions/decisions_pres_gpt-4-0314.csv', 'results/decisions_pres_gpt-4-0314')
+perform_analysis_decisions('completions/decisions_desc_gpt-4-0314.csv', 'results/decisions_desc_gpt-4-0314')
