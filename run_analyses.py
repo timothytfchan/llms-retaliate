e668a793912e88a12799d2f6717497383a526701
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2023-09-09 18:48:02 +0100

    Added analysis of human labels

--- run_analyses.py
+++ run_analyses.py
@@ -21,4 +21,7 @@ perform_analysis_completions('labels/benefits_pres_follow_up_gpt-3.5-turbo-0301.
 perform_analysis_completions('labels/benefits_desc_follow_up_gpt-4-0314.csv', 'results/benefits_desc_follow_up_gpt-4-0314', None, 'GPT-4 Follow-Up Methods (Descriptive)')
 perform_analysis_completions('labels/benefits_pres_follow_up_gpt-4-0314.csv', 'results/benefits_pres_follow_up_gpt-4-0314', None, 'GPT-4 Follow-Up Methods (Prescriptive)')
 
-perform_analysis_completions('results/methods_pres_gpt-4-0314/methods_pres_gpt-4-0314_offense_recur.csv', 'results/methods_pres_follow_up_gpt-3.5-turbo-0301', None, 'GPT-4 Methods (Prescriptive, Offense Recurrence)', 'human_labels')
+# Human labels
+perform_analysis_completions('results/methods_pres_gpt-4-0314/methods_pres_gpt-4-0314_offense_recur.csv', 'results/methods_pres_gpt-4-0314', None, 'GPT-4 Methods (Prescriptive, Offense Recurrence)', 'human_labels')
+perform_analysis_completions('results/benefits_desc_gpt-4-0314/benefits_desc_gpt-4-0314_interaction_cont.csv', 'results/benefits_desc_gpt-4-0314', None, 'GPT-4 Benefits (Descriptive, Interaction Continuity)', 'human_labels')
+perform_analysis_completions('results/benefits_pres_gpt-4-0314/benefits_pres_gpt-4-0314_deter_others.csv', 'results/benefits_pres_gpt-4-0314', None, 'GPT-4 Benefits (Prescriptive, Third Party Deterrence)', 'human_labels')
